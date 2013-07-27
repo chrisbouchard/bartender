de6493b51fafbeafc9e7badc12a99b878ae2f596
Chris Bouchard <cbou@cs.albany.edu>
2013-07-27 07:10:11 -0400

    Starting switch to new options system.

--- clients/TestClient/Main.hs
+++ clients/TestClient/Main.hs
@@ -7,7 +7,6 @@ import Control.Monad.IO.Class
 import Data.Time.Format
 import Data.Time.LocalTime
 
-import System.Console.CmdArgs
 import System.Environment
 import System.Exit
 import System.IO
@@ -17,35 +16,63 @@ import System.Log.Handler.Simple
 
 import BarTender.Client
 import BarTender.Dzen
+import BarTender.Options
 import BarTender.Timer
+import BarTender.Util
 
 -- Options to this program, gotten from the command line, a config file, or
 -- something like that
 data Options = Options
-    { host    :: String -- The server host
-    , port    :: Int    -- The server port
-    , name_   :: String -- The client name
-    , retries :: Int    -- The number of times to retry connecting
-    , timeout :: Int    -- The number of seconds to wait for server response
+    { name     :: String            -- The client name
+      help     :: Bool              -- Whether the user wants the help message
+      connOpts :: ConnectionOptions -- The client connection options
     }
-    deriving (Show, Data, Typeable)
+    deriving (Show)
+
+defaultOptions :: Options
+defaultOptions = Options
+    { name = "Test"
+      help = False
+    , connOpts = defaultConnectionOptions
+    }
 
 -- Set default options and annotations
-options :: Options
-options = Options
-    { port = 9999
-        &= help "The server port"
-    , name_ = "Test"
-        &= help "Specify a client name"
-    , retries = 0
-        &= help "Number of times to retry connecting"
-    , timeout = 30
-        &= help "Number of seconds to wait for server response"
-    , host = def &= argPos 0 &= opt "localhost"
-    }
-    &= program "TestClient"
-    &= summary "TestClient v0.1.0"
-    &= help "A test client to connect to the StatusBar server"
+-- options :: Options
+-- options = Options
+--     { port = 9999
+--         &= help "The server port"
+--     , name_ = "Test"
+--         &= help "Specify a client name"
+--     , retries = 0
+--         &= help "Number of times to retry connecting"
+--     , timeout = 30
+--         &= help "Number of seconds to wait for server response"
+--     , host = def &= argPos 0 &= opt "localhost"
+--     }
+--     &= program "TestClient"
+--     &= summary "TestClient v0.1.0"
+--     &= help "A test client to connect to the StatusBar server"
+
+optionDescriptions :: [OptDescr (Options -> Options)
+optionDescriptions =
+    [ Option ['p'] ["port"]
+        (ReqArg $ \str opts@(Options {connOpts = cOpts}) ->
+            opts { connOpts = cOpts { connectPort = str } }
+        "The server port"
+    , Option ['r'] ["retries"]
+        (ReqArg $ \str opts@(Options {connOpts = cOpts}) ->
+            opts { connOpts = cOpts { connectRetries = str } }
+        "Number of times to retry connecting"
+    , Option ['t'] ["timeout"]
+        (ReqArg $ \str opts@(Options {connOpts = cOpts}) ->
+            case maybeRead str of
+                Just n  -> Right $ opts { connOpts = cOpts { connectTimeout = n } }
+                Nothing -> Left $ "Invalid timeout '" ++ str ++ "'"
+        "Number of seconds to wait for server response"
+    , Option ['h', '?'] ["help"]
+        (NoArg $ \opts -> opts { help = True })
+        "Display this help message"
+    ]
 
 main :: IO ()
 main = do
@@ -55,7 +82,7 @@ main = do
 
     debugM "Main.main" $ "Enter"
 
-    options <- cmdArgs options
+    errorOrOptions <- cmdArgs options
 
     runClient (name_ options) $ do
         connectClient $ connectionOptions options
--- src/BarTender/Options.hs
+++ src/BarTender/Options.hs
@@ -1,11 +1,10 @@
 module BarTender.Options
-    ( OptDescr (..)
-    , ArgDescr (..)
+    ( module System.Console.GetOpt
     , FilePath
+    , completeOption
     , getConfigOpt
-    , getConfigOpt'
     , getEnvironOpt
-    , getEnvironOpt'
+    , handleOpt
     ) where
 
 import Control.Monad
@@ -23,25 +22,24 @@ import Text.ParserCombinators.Parsec
 
 import BarTender.Util
 
+completeOption :: OptDescr (Bool -> a) -> [OptDescr a]
+completeOption (Option shortLs longLs descr help) = case descr of
+        NoArg fn    -> [ Option shortLs longLs (NoArg $ fn True) help
+                       , Option [] (map ("--no-" ++) longLs) (NoArg $ fn False) ""
+                       ]
+        OptArg fn x -> [ Option shortLs longLs (OptArg (flip fn $ True) x) help ]
+        ReqArg fn x -> [ Option shortLs longLs (ReqArg (flip fn $ True) x) help ]
+
 -- | Works like @getOpt@ from "System.Console.GetOpt", except it parses a
 -- config file instead of a list of tokens. Short option names are ignored.
 getConfigOpt :: [OptDescr a]
              -> FilePath
              -> IO ([a], [String], [String])
 getConfigOpt descList path = do
-    (x, y, z, _) <- getConfigOpt' descList path
-    return (x, y, z)
-
--- | Works like @getOpt'@ from "System.Console.GetOpt", except it parses a
--- config file instead of a list of tokens. Short option names are ignored.
-getConfigOpt' :: [OptDescr a]
-              -> FilePath
-              -> IO ([a], [String], [String], [String])
-getConfigOpt' descList path = do
     errorOrPairs <- parseFromFile file path
     return $ case errorOrPairs of
-        Left error  -> ([], [], [], [show error])
-        Right pairs -> getOpt' RequireOrder descList $ foldr fn [] pairs
+        Left error  -> ([], [], [show error])
+        Right pairs -> getOpt RequireOrder descList $ foldr fn [] pairs
     where
         fn :: (String, String) -> [String] -> [String]
         fn (key, value) list = (++ list) $ case getArgDescr key descList of
@@ -60,6 +58,40 @@ getConfigOpt' descList path = do
             (Option shortLs longLs descr help) <- ls
             return $ ifJust (key `elem` longLs) descr
 
+        eol :: Parser ()
+        eol = do
+            try (void $ oneOf "\n\r") <|> eof
+            return ()
+            <?> "end of line"
+
+        comment :: Parser ()
+        comment = do
+            char '#'
+            manyTill anyChar (try eol)
+            return ()
+            <?> "comment"
+
+        item :: Parser (String, String)
+        item = do
+            key <- manyTill anyChar $ char '='
+            skipMany space
+            value <- manyTill anyChar $ try eol <|> comment
+            return (rstrip key, rstrip value)
+            where
+                rstrip :: String -> String
+                rstrip = reverse . dropWhile isSpace . reverse
+
+        line :: Parser (Maybe (String, String))
+        line = do
+            mPair <- try (comment >> return Nothing) <|> (item >>= return . Just)
+            skipMany space
+            return mPair
+
+        file :: Parser [(String, String)]
+        file = do
+            skipMany space
+            fmap catMaybes $ many line
+
 -- | Works like @getOpt@ from "System.Console.GetOpt", except it searches the
 -- environment for options instead of parsing a list of tokens. Short option
 -- names are ignored. A flag @--foo-bar@ will correspond to the environment
@@ -67,31 +99,18 @@ getConfigOpt' descList path = do
 getEnvironOpt :: [OptDescr a]
               -> IO ([a], [String], [String])
 getEnvironOpt descList = do
-    (x, y, z, _) <- getEnvironOpt' descList
-    return (x, y, z)
-
--- | Works like @getOpt'@ from "System.Console.GetOpt", except it searches the
--- environment for options instead of parsing a list of tokens. Short option
--- names are ignored. A flag @--foo-bar@ will correspond to the environment
--- variable @FOO_BAR@.
-getEnvironOpt' :: [OptDescr a]
-               -> IO ([a], [String], [String], [String])
-getEnvironOpt' descList = do
-    argList <- fmap concat . sequence $ map optionToArgs descList
-    return $ getOpt' RequireOrder descList $ argList
+    ls <- fmap catMaybes . sequence $ map optionToValue descList
+    return $ (ls, [], [])
     where
-        optionToArgs :: OptDescr a -> IO [String]
-        optionToArgs (Option shortLs longLs descr help) = do
+        optionToValue :: OptDescr a -> IO (Maybe a)
+        optionToValue (Option _ longLs descr _) = do
             mValue <- getEnvironValue longLs
             return $ case mValue of
-                Nothing    -> []
-                Just value -> let key = head longLs in
-                    case descr of
-                        NoArg _    -> ["--" ++ key]
-                        OptArg _ _ -> ["--" ++ key, value]
-                        ReqArg _ _ -> if null value
-                                          then ["--" ++ key]
-                                          else ["--" ++ key, value]
+                Nothing    -> Nothing
+                Just value -> Just $ case descr of
+                    NoArg x     -> x
+                    OptArg fn _ -> fn $ ifJust (null value) value
+                    ReqArg fn _ -> fn value
 
         getEnvironValue :: [String] -> IO (Maybe String)
         getEnvironValue keyLs = do
@@ -101,37 +120,13 @@ getEnvironOpt' descList = do
         toEnvironKey :: String -> String
         toEnvironKey = map $ toUpper . (\c -> if c == '-' then '_' else c)
 
-eol :: Parser ()
-eol = do
-    try (void $ oneOf "\n\r") <|> eof
-    return ()
-    <?> "end of line"
-
-comment :: Parser ()
-comment = do
-    char '#'
-    manyTill anyChar (try eol)
-    return ()
-    <?> "comment"
-
-item :: Parser (String, String)
-item = do
-    key <- manyTill anyChar $ char '='
-    skipMany space
-    value <- manyTill anyChar $ try eol <|> comment
-    return (rstrip key, rstrip value)
+-- | Use the results of a get*Opt function to generate either a result option
+-- value or an error message.
+handleOpt :: ([a -> Either (Maybe String) a], [String], [String])
+         -> a
+         -> (Either (Maybe String) a, [String])
+handleOpt (fnLs, nonOptLs, errorLs) initOpt = (result, nonOptLs)
     where
-        rstrip :: String -> String
-        rstrip = reverse . dropWhile isSpace . reverse
-
-line :: Parser (Maybe (String, String))
-line = do
-    mPair <- try (comment >> return Nothing) <|> (item >>= return . Just)
-    skipMany space
-    return mPair
-
-file :: Parser [(String, String)]
-file = do
-    skipMany space
-    fmap catMaybes $ many line
+        result = foldr (=<<) (Right initOpt) $
+            map (const . Left . Just) errorLs ++ fnLs
 
